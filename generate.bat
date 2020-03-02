d84a620574f78e24cc0385002723aa942799233f
helix.d <helixd.2k18@gmail.com>
2020-03-02 22:39:46 +0800

    no avoidable issue with vulkan library

--- generate.bat
+++ generate.bat
@@ -1,8 +1,9 @@
 call vcvarsall.bat x64
 call vcvars64.bat
-call java -jar javacpp.jar -cp .                                                      VKWrap/VkDeviceCreateInfoWrap.java  -header 
-call java -jar javacpp.jar -cp .;VKWrap/VkDeviceCreateInfoWrap.class                  VKWrap/JabaClass.java               -header 
-call javac                 -cp .;VKWrap/VKWrap.class;org/lwjgl/lwjgl/lwjgl.jar;org/lwjgl/lwjgl/lwjgl-vulkan.jar;org/bytedeco/javacpp/javacpp.jar  Jaba.java 
-call java                  -cp .;VKWrap/VKWrap.class;org/lwjgl/lwjgl/lwjgl.jar;org/lwjgl/lwjgl/lwjgl-vulkan.jar;org/bytedeco/javacpp/javacpp.jar  Jaba
+call java -jar javacpp.jar -cp .;                                                                                VKWrap/VkDeviceCreateInfoWrap.java  -header -d out/production
+call java -jar javacpp.jar -cp .;VKWrap/VkDeviceCreateInfoWrap.class                                             VKWrap/JabaClass.java               -header -d out/production
+call javac                 -cp .;VKWrap/VKWrap.class;org/lwjgl/lwjgl/lwjgl.jar;org/lwjgl/lwjgl/lwjgl-vulkan.jar;org/bytedeco/javacpp/javacpp.jar  Jaba.java  -d out/production
+call cd out/production
+call java                  -cp .;org/production/VKWrap/VKWrap.class;org/lwjgl/lwjgl/lwjgl.jar;org/lwjgl/lwjgl/lwjgl-vulkan.jar;org/bytedeco/javacpp/javacpp.jar  Jaba       
 ::call java -jar javacpp.jar   Jaba.java -exec :: INCOMPATIBLE WITH LWJGL!!!
 pause
